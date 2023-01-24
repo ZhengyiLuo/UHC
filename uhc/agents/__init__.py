e490050daab01ffab9c5c2973cfa2f5dc1b4c1c1
Zhengyi Luo <zhengyil@nvidia.com>
2023-01-24 18:07:55 -0500

    smpl robot

--- uhc/agents/__init__.py
+++ uhc/agents/__init__.py
@@ -1,11 +1,7 @@
 from .agent_copycat import AgentCopycat
-from .agent_copycat_sl import AgentCopycatSL
 from .agent_uhm import AgentUHM
-from .agent_uhm_goal import AgentUHMGoal
 
 agent_dict = {
     'agent_copycat': AgentCopycat,
     'agent_uhm': AgentUHM,
-    'agent_uhm_goal': AgentUHMGoal, 
-    "agent_copycat_sl": AgentCopycatSL
 }
